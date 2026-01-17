1847a5fd065f85baeae1ddae1180ea361c2300f9
Aaron Chen <nailo2c@gmail.com>
2026-01-17 11:23:10 -0800

    Update success callback log message version

--- example_git_bundle_success_dag.py
+++ example_git_bundle_success_dag.py
@@ -4,7 +4,7 @@ import logging
 
 # Add a test comment
 def _on_success(context):
-    logging.getLogger().warning(">>> SUCCESS CALLBACK TRIGGERED VERSION 2 <<<")
+    logging.getLogger().warning(">>> SUCCESS CALLBACK TRIGGERED VERSION 3 <<<")
 
 default_args = {
     "owner": "airflow",
