196508846c7322b38be2ef481842bd156f9861d8
Aaron Chen <nailo2c@gmail.com>
2026-01-17 11:46:44 -0800

    Add failure callback to DAG

--- example_git_bundle_success_dag.py
+++ example_git_bundle_success_dag.py
@@ -6,6 +6,9 @@ import logging
 def _on_success(context):
     logging.getLogger().warning(">>> SUCCESS CALLBACK TRIGGERED VERSION 3 <<<")
 
+def _on_failure(context):
+    logging.getLogger().warning(">>> FAILURE CALLBACK TRIGGERED VERSION 4 <<<")
+
 default_args = {
     "owner": "airflow",
     "retries": 0,
@@ -16,9 +19,12 @@ with DAG(
     default_args=default_args,
     schedule=None,
     on_success_callback=_on_success,
+    on_failure_callback=_on_failure,
     catchup=False,
 ) as dag:
     BashOperator(
-        task_id="always_succeed",
-        bash_command="echo 'simulating success' && exit 0",
+        # task_id="always_succeed",
+        # bash_command="echo 'simulating success' && exit 0",
+        task_id="always_fail",
+        bash_command="echo 'simulating failure' && exit 1",
     )
